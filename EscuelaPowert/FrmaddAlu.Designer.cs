02a1759b3ef5a41361c82a0c7015f11f7229356e
carlosnagomi <41878918+carlosnagomi@users.noreply.github.com>
2020-02-19 16:51:23 -0700

    .

--- EscuelaPowert/FrmaddAlu.Designer.cs
+++ EscuelaPowert/FrmaddAlu.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(411, 174);
+            this.button1.Location = new System.Drawing.Point(271, 240);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "btn";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // FrmaddAlu
