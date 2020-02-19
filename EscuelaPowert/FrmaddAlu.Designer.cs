95a0e68a1f382d6c1e316e7f28fc53f8f526d409
mr3lix <61253431+mr3lix@users.noreply.github.com>
2020-02-19 16:51:08 -0700

    .

--- EscuelaPowert/FrmaddAlu.Designer.cs
+++ EscuelaPowert/FrmaddAlu.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "btn";
+            this.button1.Text = "hola";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // FrmaddAlu
