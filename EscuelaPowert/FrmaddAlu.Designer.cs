0a510453a4a7389b9fb929e1bf386dddd4fc1b5e
carlosnagomi <41878918+carlosnagomi@users.noreply.github.com>
2020-02-19 16:38:31 -0700

    .

--- EscuelaPowert/FrmaddAlu.Designer.cs
+++ EscuelaPowert/FrmaddAlu.Designer.cs
@@ -45,6 +45,7 @@
             this.btnguardar.TabIndex = 0;
             this.btnguardar.Text = "Guardar";
             this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
             // 
             // label1
             // 
--- EscuelaPowert/FrmaddAlu.cs
+++ EscuelaPowert/FrmaddAlu.cs
@@ -16,5 +16,10 @@ namespace EscuelaPowert
         {
             InitializeComponent();
         }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
