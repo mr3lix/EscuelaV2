9bce2698b812b256aa21eff07d134198275a06ac
mr3lix <61253431+mr3lix@users.noreply.github.com>
2020-02-19 17:01:26 -0700

    .

---
+++ EscuelaPowert/FrmaddGrupo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscuelaPowert
+{
+    public partial class FrmaddGrupo : Form
+    {
+        public FrmaddGrupo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregarGrupo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Perro");
+        }
+    }
+}
